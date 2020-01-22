663455bd7d93be8c91ee989f89f3b31ba4ee8917
FaiyazMujawar <mujawar.faiyaz.aziz@gmail.com>
2020-01-22 20:56:21 +0530

    implemented some jQuery

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
-let buttonColors = ["red", "blue", "green", "yellow"];
+let buttonColor = ["red", "blue", "green", "yellow"];
+
+let level = 0;
+
+let isGameStarted = false;
+
+$(document).on("keypress", (event) => {
+    if (!isGameStarted) {
+        isGameStarted = true;
+        nextSequence();
+    }
+})
+
 let gamePattern = [];
+let userClickedPattern = [];
 
-let blue = new Audio("sounds/blue.mp3");
-let green = new Audio("sounds/green.mp3");
-let red = new Audio("sounds/red.mp3");
-
-let randomColor;
 function nextSequence() {
-    randomColor = Math.floor(Math.random() * 3) + 1;
-    gamePattern.push(randomColor);
+    level += 1;
+    $("h1").text(`Level ${level}`);
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColor = buttonColor[randomNumber];
+    gamePattern.push(randomChosenColor);
+    playSound(randomChosenColor);
+    animatePress(randomChosenColor);
 }
 
-$("#" + buttonColors[randomColor]).click(() => {
-    $("#" + buttonColors[randomColor]).toggle("pressed");
-    setTimeout(() => {
-        $("#" + buttonColors[randomColor]).toggle("pressed");
-    }, 100);
+let userChosenColor;
+
+$(".btn").click(function () {
+    userChosenColor = $(this).attr("id");
+    // $(this).fadeOut(100).fadeIn(100);
+    animatePress(userChosenColor)
+    playSound(userChosenColor);
+    userClickedPattern.push(userChosenColor)
 });
+
+function playSound(name) {
+    let audio = new Audio(`sounds/${name}.mp3`);
+    audio.play();
+}
+
+function animatePress(currentColor) {
+    $(`#${currentColor}`).toggleClass("pressed");
+    setTimeout(() => {
+        $(`#${currentColor}`).toggleClass("pressed");
+    }, 100);
+}
+
+function checkAnswer(currentLevel) {
+
+}
