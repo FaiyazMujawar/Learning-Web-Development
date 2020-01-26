1a5ed305483cfc771a368fdf244eb16ae24c91e0
FaiyazMujawar <mujawar.faiyaz.aziz@gmail.com>
2020-01-26 15:49:37 +0530

    basic external API connection

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const request = require("request");
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -8,6 +11,16 @@ let PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 })
+
+app.post("/", (req, res) => {
+    let crypto = req.body.crypto;
+    let currency = req.body.fiat;
+    //Requesting data from BitcoinAverage API fo data through request package
+    request.get(`https://apiv2.bitcoinaverage.com/indices/global/ticker/${crypto}${currency}`, (error, response, body) => {
+        let price = JSON.parse(body).last;
+        res.send(`Price in ${currency}: ${currency}${price}`);
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
