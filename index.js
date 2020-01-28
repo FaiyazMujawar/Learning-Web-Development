99334dcae811c7eb6153ecb50b064f32b94a7e69
FaiyazMujawar <mujawar.faiyaz.aziz@gmail.com>
2020-01-28 20:26:54 +0530

    commit

--- index.js
+++ index.js
@@ -4,9 +4,28 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 let PORT = process.env.PORT || 3000;
 
+let items = [];
+
+app.get("/", (req, res) => {
+    let options = {
+        weekday: "long",
+        month: "short",
+        day: "numeric"
+    }
+    let date = new Date();
+    let day = date.toLocaleDateString("en-US", options); // to convert from numeric date format to string
+    res.render("list", { day, items })
+});
+
+app.post("/", (req, res) => {
+    items.push(req.body.item);
+    res.redirect("/");
+});
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);;
-})
+    console.log(`Server running on port ${PORT}`);
+});
