775e7d39ef4cf9006a361df4d66cfca1787ac852
FaiyazMujawar <mujawar.faiyaz.aziz@gmail.com>
2020-01-31 22:11:46 +0530

    add database

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const _ = require("lodash");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const mongoose = require("mongoose");
 
-const posts = [];
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,8 +13,29 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Connecting to database
+
+const DB = require("./config/keys").MongoURI;
+mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log("Connected to database"))
+	.catch(error => console.log(error));
+
+// Requiring Model
+
+const Post = require("./models/Post");
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
 app.get("/", (req, res) => {
-	res.render("home", { content: homeStartingContent, posts });
+	Post.find({}, (error, posts) => {
+		if (error) {
+			console.log(error);
+		} else {
+			res.render("home", { content: homeStartingContent, posts });
+		}
+	});
 });
 
 app.get("/about", (req, res) => {
@@ -29,27 +48,33 @@ app.get("/contact", (req, res) => {
 
 
 app.get("/compose", (req, res) => {
-	res.render("compose", {});
+	res.render("compose");
 });
 
 app.post("/compose", (req, res) => {
 	const post = {
-		title: req.body.postTitle,
+		title: _.capitalize(req.body.postTitle),
 		body: req.body.post
 	};
-	posts.push(post);
-	res.redirect("/");
-});
-
-app.get("/posts/:postTitle", (req, res) => {
-	posts.forEach(post => {
-		if (_.lowerCase(post.title) === _.lowerCase(req.params.postTitle)) {
-			res.render("post", { post });
+	Post.insertMany([post], (error) => {
+		if (error) {
+			console.log(error);
 		}
 	});
 	res.redirect("/");
 });
 
-app.listen(3000, function () {
+app.get("/posts/:postId", (req, res) => {
+	const postId = _.capitalize(req.params.postId);
+	Post.findOne({ _id: postId }, (error, post) => {
+		if (error) {
+			console.log(error);
+		} else {
+			res.render("post", { post });
+		}
+	});
+});
+
+app.listen(PORT, function () {
 	console.log("Server started on port 3000");
 });
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: "mongodb+srv://fayaz99:fayaz@99@cluster0-uzd5f.mongodb.net/todolist"
+}
