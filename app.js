8b4c8c5260a5d3802def284c5941ea70301b9a87
FaiyazMujawar <mujawar.faiyaz.aziz@gmail.com>
2020-02-01 13:15:37 +0530

    complete level 2 encryption

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -19,9 +21,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "qwertyuiop[]";
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = mongoose.model("User", userSchema);
 
